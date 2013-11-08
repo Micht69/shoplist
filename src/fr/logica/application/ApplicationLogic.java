7565de2b96fcb38ad3bbdcc5c8233e6a300e3380
Sébastien SCHMITT <sebastien.schmitt@logica.com>
2013-11-08 14:30:23 +0100

    Disable login Default page to shop_list Remove en lang

--- src/fr/logica/application/ApplicationLogic.java
+++ src/fr/logica/application/ApplicationLogic.java
@@ -1,5 +1,12 @@
 package fr.logica.application;
 
+import javax.faces.context.FacesContext;
+
+import fr.logica.domain.models.ShopListModel;
+import fr.logica.jsf.controller.JSFController;
+import fr.logica.jsf.utils.JSFBeanUtils;
+import fr.logica.queries.ShopListQuery;
+import fr.logica.security.ApplicationUser;
 
 /**
  * Class used to store application logic methods.
@@ -9,4 +16,10 @@ public class ApplicationLogic extends DefaultApplicationLogic {
 	/** SerialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
+	@Override
+	public String getDefaultPage(ApplicationUser user) {
+		return ((JSFController) JSFBeanUtils.getManagedBean(FacesContext.getCurrentInstance(), "jsfCtrl"))
+				.prepareList(ShopListModel.ENTITY_NAME, ShopListQuery.Query.QUERY_SHOP_LIST, "SHOP_LIST_LIST");
+
+	}
 }
